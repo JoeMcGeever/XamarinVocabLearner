226d993a15d958feb741f2605d7ba6f3d856b1ce
Joseph McGeever <mcgeevej@uni.coventry.ac.uk>
2020-11-18 10:58:54 +0000

    Pic can be taken + shows

--- VocabLearner/VocabLearner/SignUpPage.xaml.cs
+++ VocabLearner/VocabLearner/SignUpPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Essentials;
 using Xamarin.Forms.Xaml;
+using Plugin.Media;
 
 namespace VocabLearner
 {
@@ -19,6 +20,50 @@ namespace VocabLearner
         public SignUpPage()
         {
             InitializeComponent();
+
+            //Initialise the takePhoto and pickPhoto buttons
+
+            takePhoto.Clicked += async (sender, args) =>
+            {
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                    return;
+                }
+
+                var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+                if (photo != null)
+                    image.Source = ImageSource.FromStream(() => { return photo.GetStream();
+                    });
+            };
+
+
+            pickPhoto.Clicked += async (sender, args) =>
+                {
+                    if (!CrossMedia.Current.IsPickPhotoSupported)
+                    {
+                        await DisplayAlert("Photos Not Supported", ":( Permission not granted to photos.", "OK");
+                        return;
+                    }
+                    var file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                    {
+                        PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+                    });
+
+
+                    if (file == null)
+                        return;
+
+                    image.Source = ImageSource.FromStream(() =>
+                    {
+                        var stream = file.GetStream();
+                        file.Dispose();
+                        return stream;
+                    });
+                };
+
         }
 
 
@@ -28,11 +73,6 @@ namespace VocabLearner
             Application.Current.MainPage.Navigation.PopAsync();
         }
 
-
-        private async void Capture_OnClicked(object sender, EventArgs e)
-        {
-        
-        }
 
 
         private async void SignUp_OnClicked(object sender, EventArgs e)
